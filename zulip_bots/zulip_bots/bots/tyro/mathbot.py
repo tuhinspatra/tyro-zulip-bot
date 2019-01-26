c2e90a03231a4b61436d4c4c7f3dae82debdc714
jarvisdev <bavishimilanchamp@gmail.com>
2019-01-26 18:25:07 +0530

    modified mathbot.py

--- zulip_bots/zulip_bots/bots/tyro/mathbot.py
+++ zulip_bots/zulip_bots/bots/tyro/mathbot.py
@@ -1,31 +1,33 @@
-import sys, os
-sys.path.insert(0,os.getcwd())
+import sys
+import os
+sys.path.insert(0, os.getcwd())
 
-import requests,json
-from typing import Dict,Any
+import requests
+import json
+from typing import Dict, Any
 
 # class mathHandler(object):
 #     def usage(self):
 #         return '''
 #             Get your mathematics expressions evaluated quickly :)
 #         '''
-def evaluate_all_expression(message:Dict[str,str],bot_handler:Any):
-    print(message['content'])
-    data=message['content'].split()
-    if data[0]=="evaluate":
-        expressions=data[1:]
-        query={"expr":expressions,"precision":10}
-        endpoint="http://api.mathjs.org/v4/"
-        response = requests.post(endpoint,data=json.dumps(query))
+
+
+def evaluate_all_expression(data):
+    if data[1] == "evaluate":
+        expressions = data[2:]
+        query = {"expr": expressions, "precision": 10}
+        endpoint = "http://api.mathjs.org/v4/"
+        response = requests.post(endpoint, data=json.dumps(query))
         # print(response.json())
         answers = response.json()
-        if answers['error']!=None:
+        if answers['error'] != None:
             # bot_handler.send_reply(message,answers['error'])
             return answers['error']
         else:
-            ret=""
+            ret = ""
             for ans in answers['result']:
-                ret=ret+ans+'\n\n'
+                ret = ret+ans+'\n\n'
             return ret
             # bot_handler.send_reply(message,ret)
 # handler_class = mathHandler
